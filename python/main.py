17699030768543ad8171c92ff2c48cbdb0a8bc04
Pablo Bartolomé Bernáldez <pablo_kss@hotmail.com>
2020-10-11 17:52:32 +0200

    Prueba: python - Apartado: 3

--- python/main.py
+++ python/main.py
@@ -3,6 +3,17 @@ from models.trabajador import Trabajador
 
 
 def main():
+    '''
+        Pregunta 3: ¿Que diferencia hay entre self.nombre y la variable
+        llamada nombre?
+
+        self.nombre es una propiedad dentro del objeto Persona, es decir, solo
+        existe dentro de esa clase  mientras que el scope de la variable nombre
+        dependera de donde se genere, en este caso al generarse en una funcion,
+        existira solo dentro de esta funcion. Si, por ejemplo, se crease la
+        variable fuera de la función esta seria una variable global dentro de
+        la clase main.py
+    '''
     nombre = 'Alberto'
     persona_1 = Persona(nombre, 20)
     persona_1.presentation()
